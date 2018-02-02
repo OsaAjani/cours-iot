6af16d71aaafab88f2970793841a0ec763c29fa2
osaajani <pierre.lin@free.fr>
2018-02-03 00:50:36 +0100

    Add create database part to mysql file

--- whatsmyip/whatsmyip.sql
+++ whatsmyip/whatsmyip.sql
@@ -1,7 +1,11 @@
+CREATE DATABASE IF NOT EXISTS whatsmyip;
+
+use whatsmyip;
+
 CREATE TABLE ips (
     id INT AUTO_INCREMENT PRIMARY KEY,
     public_ip VARCHAR(255) NOT NULL,
     local_ip VARCHAR(255) NOT NULL,
     hostname VARCHAR(255) NOT NULL,
     at DATETIME
-) 
+) ;
