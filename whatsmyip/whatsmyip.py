5357ad4495d27947bfa3c51fc55f82b8e6e99799
osaajani <pierre.lin@free.fr>
2018-02-03 01:19:33 +0100

    Add interface variable for python script

--- whatsmyip/whatsmyip.py
+++ whatsmyip/whatsmyip.py
@@ -17,9 +17,10 @@ def get_ip_address(ifname):
     )[20:24])
 
 server_script_url = 'http://plebweb.fr/cours-iot/whatsmyip.php'
+interface_name = 'wlan0'
 
 hostname = socket.gethostname()
-local_ip = get_ip_address('wlan0')
+local_ip = get_ip_address(interface_name)
 
 request_url = server_script_url + '?local_ip=' + local_ip + '&hostname=' + hostname
 
