92191437ece46ce13dc149c3f9e9a7a6e92825f0
osaajani <pierre.lin@free.fr>
2018-02-14 18:32:03 +0100

    Add file for create example database

---
+++ introduction_a_python/create_database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS cours_iot_introduction_python;
+use cours_iot_introduction_python;
+
+CREATE TABLE  IF NOT EXISTS user (
+    id INT NOT NULL AUTO_INCREMENT,
+    firstname VARCHAR(200),
+    lastname VARCHAR(200),
+    age INT,
+    PRIMARY KEY (id)
+)
