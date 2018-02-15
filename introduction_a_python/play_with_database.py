79db410df154d00ae3a052ae715619742ba2ea00
osaajani <pierre.lin@free.fr>
2018-02-15 02:09:03 +0100

    Add play with database

--- introduction_a_python/play_with_database.py
+++ introduction_a_python/play_with_database.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3.5
 #-*- coding: utf-8 -*-
 import mysql.connector
-mysql_connection = mysql.connector.connect(host = 'localhost', user = 'root', password = 'your_password', database = 'cours_iot_introduction_python')
+mysql_connection = mysql.connector.connect(host = 'localhost', user = 'root', password = 'root', database = 'cours_iot_introduction_python')
 mysql_cursor = mysql_connection.cursor()
-query_data = {'firstname': 'Bernard', 'lastname' : 'Tessier', 'age' : 55}
-mysql_cursor.execute('INSERT INTO user (firstname, lastname, age) VALUES (\'{firstname}\', \'{lastname}\', {age})'.format(query_data))
 
+mysql_cursor.execute('SELECT * FROM user')
+users = mysql_cursor.fetchall()
+
+for user in users :
+    print('id: {}, firstname: {}, lastname: {}, age: {}'.format(user[0], user[1], user[2], user[3]))
+
+query_data = {'firstname': 'Elisabeth', 'lastname' : 'Tessier', 'age' : 80}
+mysql_cursor.execute('INSERT INTO user (firstname, lastname, age) VALUES (%(firstname)s, %(lastname)s, %(age)s)', query_data)
+mysql_connection.commit()
